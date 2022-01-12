5728690fb42c1ed781e6e234034fe470323a0f43
akardos11 <akardos11@gmail.com>
2022-01-12 18:52:10 +0100

    Add threds

--- src/main/java/Connect.java
+++ src/main/java/Connect.java
@@ -1,7 +1,14 @@
+
+
+
+import lombok.extern.slf4j.Slf4j;
+
 import java.sql.*;
+
 
 public class Connect {
 
+    @Slf4j
     public static class Main {
 
         private String url = "jdbc:postgresql://localhost/test";
@@ -20,79 +27,109 @@ public class Connect {
 
             try (Connection conn = this.connect();
                  Statement stmt = conn.createStatement();
-                ResultSet rs = stmt.executeQuery(SQL);){
+                 ResultSet rs = stmt.executeQuery(SQL);) {
 
                 while (rs.next()) {
                     //Display values
-                    System.out.print("ID: " + rs.getInt("id_con"));
-                    System.out.print(", First Name: " + rs.getString("first_name"));
-                    System.out.print(", Last Name: " + rs.getString("last_name"));
-                    System.out.println(", Email: " + rs.getString("email"));
+                    log.info(", ID: " + rs.getInt("id_con"));
+                    log.info(", First Name: " + rs.getString("first_name"));
+                    log.info(", Last Name: " + rs.getString("last_name"));
+                    log.info(", Email: " + rs.getString("email"));
                 }
-            }catch (SQLException e){
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
 
 
         }
 
-     public long insertCon(Contacts contacts) {
+        public long insertCon(Contacts contacts) {
 
-         String SQL = "INSERT INTO contacts(first_name,last_name,email) "
-                 + "VALUES(?,?,?)";
+            String SQL = "INSERT INTO contacts(first_name,last_name,email) "
+                    + "VALUES(?,?,?)";
 
-         long id = 0;
-         try (Connection conn = connect();
-              PreparedStatement pstmt = conn.prepareStatement(SQL,
-                      Statement.RETURN_GENERATED_KEYS)) {
+            long id = 0;
+            try (Connection conn = connect();
+                 PreparedStatement pstmt = conn.prepareStatement(SQL,
+                         Statement.RETURN_GENERATED_KEYS)) {
 
-             pstmt.setString(1, contacts.getFirst_name());
-             pstmt.setString(2, contacts.getLast_name());
-             pstmt.setString(3, contacts.getEmail());
-             int affectedRows = pstmt.executeUpdate();
-             if (affectedRows > 0){
-                 try(ResultSet rs = pstmt.getGeneratedKeys()){
-                     if(rs.next()){
-                         id = rs.getLong(1);
-                     }
-                 }catch (SQLException ex){
-                     System.out.println(ex.getMessage());
-                 }
-             }
-
-
-         } catch (SQLException throwables) {
-             throwables.printStackTrace();
-         }
+                pstmt.setString(1, contacts.getFirst_name());
+                pstmt.setString(2, contacts.getLast_name());
+                pstmt.setString(3, contacts.getEmail());
+                int affectedRows = pstmt.executeUpdate();
+                if (affectedRows > 0) {
+                    try (ResultSet rs = pstmt.getGeneratedKeys()) {
+                        if (rs.next()) {
+                            id = rs.getLong(1);
+                        }
+                    } catch (SQLException ex) {
+                        log.info(ex.getMessage());
+                    }
+                }
 
 
-         return id;
-     }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
 
-     public int deleteContacts(int id_con){
+
+            return id;
+        }
+
+        public int deleteContacts(int id_con) {
             String SQL = "DELETE FROM contacts WHERE id_con =?";
             int affectedrows = 0;
 
-         try (Connection conn = connect();
-              PreparedStatement pstmt = conn.prepareStatement(SQL)) {
+            try (Connection conn = connect();
+                 PreparedStatement pstmt = conn.prepareStatement(SQL)) {
 
-             pstmt.setInt(1, id_con);
+                pstmt.setInt(1, id_con);
 
-             affectedrows = pstmt.executeUpdate();
+                affectedrows = pstmt.executeUpdate();
 
-         } catch (SQLException ex) {
-             System.out.println(ex.getMessage());
-         }
-         return affectedrows;
-     }
+            } catch (SQLException ex) {
+                log.info(ex.getMessage());
+            }
+            return affectedrows;
+        }
 
 
-        public static void main (String[]args){
-            Main main = new Main();
-            Contacts contacts = new Contacts(1,"Adam","Słony","@wp.pl");
-            long id = main.insertCon(contacts);
-            main.deleteContacts(5);
-            main.getContacts();
+        public static void main(String[] args) {
+            Thread thread = new Thread(() -> {
+                Main main = new Main();
+                while (true) {
+                    Contacts contacts = new Contacts(1, "Adam", "Słony", "@wp.pl");
+                    long id = main.insertCon(contacts);
+                    main.getContacts();
+                }
 
-        }}}
+
+            });
+            Thread thread2 = new Thread(() -> {
+                Main main = new Main();
+                while (true) {
+                    Contacts contacts = new Contacts(1, "Adam", "Słony", "@wp.pl");
+                    long id = main.insertCon(contacts);
+
+                }
+
+
+            });
+
+            Thread thread3 = new Thread(() -> {
+                Main main = new Main();
+                while (true) {
+                    Contacts contacts = new Contacts(1, "Adam", "Słony", "@wp.pl");
+                    long id = main.insertCon(contacts);
+
+                }
+
+
+            });
+            thread.start();
+            thread2.start();
+            thread3.start();
+        }
+    }
+}
 
--- src/main/java/Contacts.java
+++ src/main/java/Contacts.java
@@ -1,3 +1,6 @@
+import lombok.Data;
+
+@Data
 public class Contacts {
 
     private int id_con;
@@ -15,38 +18,5 @@ public class Contacts {
         this.email = email;
     }
 
-    public Contacts(){
 
-    }
-    public int getId_con() {
-        return id_con;
-    }
-
-    public void setId_con(int id) {
-        this.id_con = id_con;
-    }
-
-    public String getFirst_name() {
-        return first_name;
-    }
-
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
-    }
-
-    public String getLast_name() {
-        return last_name;
-    }
-
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
